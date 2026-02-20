46c1e7a6920ed7ad633cd489ca85ffa660b0198b
CURSOS ORIGAMID <paulocursosorigamid@gmail.com>
2026-02-20 15:00:43 -0300

    feat(scss):create new scss structure to migrate from tailwind to scss dark mode on final-dashbaord.v2

--- src/components/dashboards/charts/finance-charts/FinanceChart.tsx
+++ src/components/dashboards/charts/finance-charts/FinanceChart.tsx
@@ -14,17 +14,17 @@ import styles from "./financeChart.module.scss";
 
 const data = [
   { name: "Jan", income: 4000, expense: 2400 },
-  { name: "Feb", income: 3000, expense: 1398 },
+  { name: "Fev", income: 3000, expense: 1398 },
   { name: "Mar", income: 2000, expense: 9800 },
-  { name: "Apr", income: 2780, expense: 3908 },
-  { name: "May", income: 1890, expense: 4800 },
+  { name: "Abr", income: 2780, expense: 3908 },
+  { name: "Mai", income: 1890, expense: 4800 },
   { name: "Jun", income: 2390, expense: 3800 },
   { name: "Jul", income: 3490, expense: 4300 },
-  { name: "Aug", income: 3490, expense: 4300 },
-  { name: "Sep", income: 3490, expense: 4300 },
-  { name: "Oct", income: 3490, expense: 4300 },
+  { name: "Ago", income: 3490, expense: 4300 },
+  { name: "Set", income: 3490, expense: 4300 },
+  { name: "Out", income: 3490, expense: 4300 },
   { name: "Nov", income: 3490, expense: 4300 },
-  { name: "Dec", income: 3490, expense: 4300 },
+  { name: "Dez", income: 3490, expense: 4300 },
 ];
 
 const FinanceChart = () => {
